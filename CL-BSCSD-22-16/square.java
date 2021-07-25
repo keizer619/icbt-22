20a998569f4710d133b13f40bd8cc84a51607cbf
rasinduramanayake <75307294+rasinduramanayake@users.noreply.github.com>
2021-07-25 11:30:44 +0530

    add star pattern

--- CL-BSCSD-22-16/square.java
+++ CL-BSCSD-22-16/square.java
@@ -12,8 +12,8 @@ package com.mycompany.mavenproject1;
 public class square {
     public static void main(String[] args)
     {  
-           for(int i=0;i<5;i++)	  
-           System.out.println("***** ");
+          for(int i=0;i<5;i++)	  
+          System.out.println("***** ");
     }
     
 }
