17a8959cb522312385f238055a8ad58cc86b473a
Abshalom Judah <judahchris99@gmail.com>
2021-07-25 11:15:08 +0530

    Updated Start Pattern

--- CL-BSCSD-22-44/project1/src/main/java/com/mycompany/project1/Run.java
+++ CL-BSCSD-22-44/project1/src/main/java/com/mycompany/project1/Run.java
@@ -10,7 +10,9 @@ package com.mycompany.project1;
  * @author ASUS
  */
 public class Run {
+    
      public static void main(String[] args) {
-        System.out.println("Hello"); 
+          for(int i=0;i<5;i++)	  
+             System.out.println("***** ");
     }
 }
