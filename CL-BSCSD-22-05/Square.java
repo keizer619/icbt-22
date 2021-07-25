f800e933785bb8b2361eb70d6fe09e30f6134a9c
Kavidu Aloka Kodikara <kavidualoka@Kavidu-Alokas-MacBook-Pro.local>
2021-07-25 11:01:14 +0530

    Adding square pattern

---
+++ CL-BSCSD-22-05/Square.java
@@ -0,0 +1,17 @@
+class Square {
+    private void main(String[] args) {
+        int x = 0, y = 0;
+
+        while(x < 4) {
+            y = 0;
+            while(y < 4) {
+                System.out.print("*");
+
+                ++y;
+            }
+
+            System.out.print("\n");
+            ++x;
+        }
+    }
+}
