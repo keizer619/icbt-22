810322ae59b868d95a14a33dd10ceb3db350d2c0
arjuna-dave1218 <arjunadeve@gmail.com>
2021-07-25 11:12:10 +0530

    ProjectAdded

--- CL-BSCSD-22-51/davesProject/src/main/java/first/NewClass.java
+++ CL-BSCSD-22-51/davesProject/src/main/java/first/NewClass.java
@@ -12,5 +12,13 @@ package first;
 public class NewClass {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print("*");
+                
+            }
+            System.out.println("");
+        }
     }
 }
