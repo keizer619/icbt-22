60ed06eb98b8af5979be45d8026410b94e162207
CL-BSCSD-22-15 <bilaal9191@gmail.com>
2021-07-25 11:03:26 +0530

    Include Project1

---
+++ CL-BSCSD-22-15/project1/src/main/java/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Bilaal
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hellow World!!!");
+        System.out.println("* + * + * + *");
+    }
+}
