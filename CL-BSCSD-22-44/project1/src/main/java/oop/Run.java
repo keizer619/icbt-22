7ce5e149ce59eafd7ea9a4611c5508472a062a50
Abshalom Judah <judahchris99@gmail.com>
2021-08-08 10:05:13 +0530

    Web Project

---
+++ CL-BSCSD-22-44/project1/src/main/java/oop/Run.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project1;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Run {
+    
+     public static void main(String[] args) {
+          for(int i=0;i<5;i++)	  
+             System.out.println("***** ");
+    }
+}
