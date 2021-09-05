e18a091cceffe915e29cde78d94a54d9266de6bc
Tharik Kanaka <tharik.kanaka@gmail.com>
2021-09-05 10:47:14 +0530

    Include rest application

---
+++ tharik/myrestservice/src/main/java/org/icbt/myrestservice/MyService.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.icbt.myrestservice;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author tharik
+ */
+@Path("/my-service")
+public class MyService {
+    @GET
+    @Path("/{message}")
+    public Response getMessage(@PathParam("message") String msg){
+       String message = "Hello " + msg;
+       return Response.status(200).entity(message).build();
+    }
+}
